9315cd36ccaf8d6e5fda61b7b87907702056842a
Prakash Aryal <prakasbabuaryal@gmail.com>
2023-09-26 11:59:22 -0400

    skeleton

---
+++ product-recommendation/src/main/java/com/product/reco/ProductRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.product.reco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRecommendationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductRecommendationApplication.class, args);
+    }
+
+}
